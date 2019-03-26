bf60690759b35b668733776dbed8dbcc4d76e127
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-03-26 22:12:11 +0430

    first model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name=       models.CharField(max_length=200 , help_text="Enter a book genre")
+
+    def __str__(self):
+        return self.name
