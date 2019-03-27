b4cd972f773a091976555bfb7fe1d116618d0904
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-03-27 17:25:52 +0430

    view_03

--- app/views.py
+++ app/views.py
@@ -5,11 +5,11 @@ from . import models
 def listview(request):
     context = {'latest_book_list' : models.Book.objects.all(),
     'num_books' : models.Book.objects.all().count(),
-    'num_authors':models.Author.objects.all().count(),
-    'num_instances_available':models.BookInstance.objects.all().count(),
-    'num_instances':models.BookInstance.objects.filter(status__exact='a').count()
+    'num_authors': models.Author.objects.all().count(),
+    'num_instances_available': models.BookInstance.objects.all().count(),
+    'num_instances': models.BookInstance.objects.filter(status__exact='a').count()
     }
-
+  
     return render(request,'app/book.html' , context)
 
 def detail(request,pk):
