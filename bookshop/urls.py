963430811f1959ce62ecf2f1ca730967dcefe2ad
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-03-27 11:44:37 +0430

    urls_02

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^book/', include('app.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
