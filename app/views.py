e2636b99017d5cd877bc0ee20a9b4641ad95419a
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-03-27 13:03:43 +0430

    view_01

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def listview(request):
-    return HttpResponse("testt")
+    context = {'latest_book_list' : models.Book.objects.all(),
+    'num_books' : models.Book.objects.all().count(),
+    'num_authors':models.Author.objects.all().count(),
+    'num_instances_available':models.BookInstance.objects.all().count(),
+    'num_instances':models.BookInstance.objects.filter(status__exact='a').count()
+    }
+
+    return render(request,'app/book.html' , context)
 
 def detail(request,pk):
     return HttpResponse("booook" + str(pk))
