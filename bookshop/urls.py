eddda63b8866529f155ef749e25b0c3d61c55d08
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-03-29 20:28:33 +0430

    ggg

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -20,7 +20,10 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^book/', include('app.urls'))
+    url(r'^book/', include('app.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    url(r'^accounts/',include('django.contrib.auth.urls')),
+]
